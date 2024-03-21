d3619d09f6d63440487bb08e35dbc7ce24d3a1c8
alc080341 <chambersltony80@gmail.com>
2024-03-21 10:07:34 +0000

    1.0.0: add column search

--- src/components/DynamicUITable/index.js
+++ src/components/DynamicUITable/index.js
@@ -1,11 +1,53 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./dynamic-ui-table.css";
 
 export default function DynamicUITable(props) {
   const [headers, updateHeaders] = useState(props.headers || []);
   const [data, updateData] = useState(props.data || []);
   const [isDraggingOver, setIsDraggingOver] = useState(false);
+  const [sortCols, setSortColIndex] = useState([]);
+
+  // ### SORT FUNCTIONS
+  useEffect(() => {
+    // ### SET THE SEARCHES
+    let createdSortCols = [];
+    for (let i in headers) {
+      createdSortCols.push({ colIndex: headers[i].index, sortOrder: "asc" });
+    }
+    setSortColIndex(createdSortCols);
+  }, []);
+
+  const sortColumn = (index) => {
+    if (headers) {
+      const accessor = headers[index].accessor;
+      let descending = false;
+      let newCurrentSortCols = sortCols.map((sc) => {
+        if (sc.colIndex === index) {
+          sc.sortOrder = sc.sortOrder === "asc" ? "desc" : "asc";
+          if (sc.sortOrder === "desc") descending = true;
+        }
+        return sc;
+      });
+
+      setSortColIndex(newCurrentSortCols);
+
+      let newData = data.sort((a, b) => {
+        let sortVal = 0;
+        if (a[accessor] < b[accessor]) {
+          sortVal = -1;
+        } else if (a[accessor] > b[accessor]) {
+          sortVal = 1;
+        }
+
+        if (descending) {
+          sortVal = sortVal * -1;
+        }
+        return sortVal;
+      });
+      updateData(newData);
+    }
+  };
 
   // ### DRAGGABLE FUNCTIONS
   const onDragStart = (e, i) => {
@@ -41,6 +83,7 @@ export default function DynamicUITable(props) {
         onDragOver={onDragOver}
         onDragDrop={onDragDrop}
         isDraggingOver={isDraggingOver}
+        sortColumn={sortColumn}
       />
     </div>
   );
@@ -61,52 +104,55 @@ function CreateTable(props) {
   let data = props.data;
   let isDraggingOver = props.isDraggingOver;
   let dragOverStyle = {
-    border: isDraggingOver ? '5px dashed #ccc' : '5px solid transparent',
-    backgroundColor: isDraggingOver ? '#f0f0f0' : 'transparent', // Apply background color when dragging over
-  }
+    border: isDraggingOver ? "5px dashed #ccc" : "5px solid transparent",
+    backgroundColor: isDraggingOver ? "#f0f0f0" : "transparent", // Apply background color when dragging over
+  };
 
-  const keysToOmit = ["index"];
   return (
     <>
-    <table className="dynamic-ui-table">
-      <thead>
-        <tr>
-          {headers.map((hdr) => (
-            <th
-              key={hdr.title + "hdr"}
-              onDragStart={(e) => onDragStart(e, hdr.index)}
-              onDragOver={(e) => onDragOver(e)}
-              onDrop={(e) => onDragDrop(e, hdr.index)}
-              index={hdr.index}            >
-              <span style={dragOverStyle}
-                className="dynamic-table-title" draggable>
-                <strong>{hdr.title}</strong>
-              </span>
-              <span>SRCH</span>
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody className="dynamic-ui-table-inner">
-        {data.map((item) => {
-          let { index, ...newObj } = item;
-          return (
-            <tr key={item.index + "-tr"}>
-              {headers.map((hdr) => (
-                <td
-                  key={item[hdr.accessor] + "-" + item.index}
-                  index={item.index}
+      <table className="dynamic-ui-table">
+        <thead>
+          <tr>
+            {headers.map((hdr) => (
+              <th
+                key={hdr.title + "hdr"}
+                onDragStart={(e) => onDragStart(e, hdr.index)}
+                onDragOver={(e) => onDragOver(e)}
+                onDrop={(e) => onDragDrop(e, hdr.index)}
+                index={hdr.index}
+              >
+                <span
+                  style={dragOverStyle}
+                  className="dynamic-table-title"
+                  draggable
                 >
-                  <span>
-                    <p>{item[hdr.accessor]}</p>
-                  </span>
-                </td>
-              ))}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                  <strong>{hdr.title}</strong>
+                </span>
+                <span onClick={() => props.sortColumn(hdr.index)}>SRCH</span>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="dynamic-ui-table-inner">
+          {data.map((item) => {
+            let { index, ...newObj } = item;
+            return (
+              <tr key={item.index + "-tr"}>
+                {headers.map((hdr) => (
+                  <td
+                    key={item[hdr.accessor] + "-" + item.index}
+                    index={item.index}
+                  >
+                    <span>
+                      <p>{item[hdr.accessor]}</p>
+                    </span>
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
